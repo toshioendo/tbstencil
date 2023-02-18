5e57f857ad0b556e2e71cff6e186f5815d1fc2a7
Toshio Endo <endo@is.titech.ac.jp>
2023-02-18 17:01:28 +0900

    developing

--- perf.sh
+++ perf.sh
@@ -3,7 +3,7 @@ export OMP_NUM_THREADS=56
 
 for n in 4 8 16 32; do
   for bt in 1 2 5 ; do
-    gcc -O2 -g -fopenmp -DNX=$((1000*n)) -DNY=$((1000*n)) -DBT=$((bt)) comp-v2.c
+    gcc -O2 -g -fopenmp -DNX=$((1000*n)) -DNY=$((1000*n)) -DBT=$((bt)) comp-v3.c
     perf stat -e LLC-loads,LLC-load-misses,L1-dcache-loads,L1-dcache-load-misses ./a.out
   done
 done
